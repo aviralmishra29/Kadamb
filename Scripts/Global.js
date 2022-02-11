fe22eb35bd0ed241dd2d8964d7264d3fa2ee6149
Aviral Mishra <onstermonster0.1@gmail.com>
2022-02-11 21:32:37 +0530

    preloder added

---
+++ Scripts/Global.js
@@ -0,0 +1,16 @@
+// This is preloder
+var startTime = new Date().getTime();
+window.scrollTo(0, 0);
+
+var preloader = document.querySelectorAll(".preloader")[0];
+var preloaderlayer = document.querySelectorAll(".preloaderlayer")[0];
+
+window.addEventListener("load",()=>{
+    while(true){
+        if((new Date().getTime()) -(startTime) > 4000){
+            preloader.style.display = "none";
+            preloaderlayer.style.display = "none";
+            break;
+        }
+    }
+})
