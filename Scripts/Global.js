d5a059328e14fda33b558a8582ccef9a87696f68
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2022-02-26 21:50:14 +0530

    routing in navbar correct of home page

--- Scripts/Global.js
+++ Scripts/Global.js
@@ -74,3 +74,13 @@ startSlide();
 // ************ Navbar button toggle *****************
 
 
+var hash = document.location.hash;
+var prefix = "tab_";
+if (hash) {
+    $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+} 
+
+// Change hash for page-reload
+$('.nav-tabs a').on('shown', function (e) {
+    window.location.hash = e.target.hash.replace("#", "#" + prefix);
+});
